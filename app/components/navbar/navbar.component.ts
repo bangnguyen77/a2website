290b6f357f3286b0167a8fd3ef0513675ff19fb8
Bang Nguyen <bangnv77@gmail.com>
2016-11-28 11:19:15 -0500

    create html files for corresponding components

--- app/components/navbar/navbar.component.ts
+++ app/components/navbar/navbar.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
+    moduleId: module.id,
     selector: 'navbar',
-    template: 'NAVBAR'
+    templateUrl: 'navbar.component.html'
 })
 export class NavbarComponent { }
